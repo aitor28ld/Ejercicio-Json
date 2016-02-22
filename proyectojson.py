ec1565e54d6ef46737ea836d8d667473e1d8221d
aitor28ld <ldaitor28@gmail.com>
2016-02-22 12:10:00 +0100

    Agregado menú a mostrar en el fichero proyectojsn.py

---
+++ proyectojson.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print "1.- Listar los nombres de los monumentos"
+print "2.- ¿Cuántos yacimientos arqueológicos hay?"
+print "3.- Pide por teclado un nombre de monumento y muestra las coordenadas de ubicación"
+print "4.- Pide por teclado una cadena y muestra las coincidencias encontradas en la descripción del monumento"
+print "5.- Pide por teclado un precio minimo y maximo (también puede ser gratuito) y muestra aquellos monumentos con un precio entre dicho rango además de que el programa preguntará si quiere información acerca de un monumento en concreto."
+
+
+op = int(raw_input("\nElige una opción numérica de las indicadas: "))
